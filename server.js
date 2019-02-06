8d16383b5838f83be4b272c7b139c83520425b71
nikhil sulegaon <nikhilsulegaon@gmail.com>
2019-02-06 12:55:55 -0700

    Making app deployable to heroku.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var index = require('./routes/index');
 app.use('/', index);
 
 
-var port = 4000;
+var port = process.env.PORT || 5000;
 app.listen(port, function () {
     console.log('Server running on http://localhost:' + port)
 });
